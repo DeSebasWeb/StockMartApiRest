d08c6d3fa4a219beec82ddf23f4aa48457854bcd
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-06-05 16:10:38 -0500

    initial commits

---
+++ src/main/java/com/stockmart/api/stockmart/StockmartApplication.java
@@ -0,0 +1,13 @@
+package com.stockmart.api.stockmart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockmartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StockmartApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stockmart/api/stockmart/StockmartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stockmart.api.stockmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
