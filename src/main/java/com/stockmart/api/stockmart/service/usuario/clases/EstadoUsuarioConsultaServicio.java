b604e49e6ee1af07584495682279f15a66669e8a
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-06-09 21:35:47 -0500

    Add the logic on the method

--- src/main/java/com/stockmart/api/stockmart/service/usuario/clases/EstadoUsuarioConsultaServicio.java
+++ src/main/java/com/stockmart/api/stockmart/service/usuario/clases/EstadoUsuarioConsultaServicio.java
@@ -1,15 +1,20 @@
 package com.stockmart.api.stockmart.service.usuario.clases;
 
 import com.stockmart.api.stockmart.entity.usuario.EstadoUsuario;
+import com.stockmart.api.stockmart.repository.usuario.EstadoUsuarioRepositorio;
 import com.stockmart.api.stockmart.service.usuario.interfaces.IEstadoUsuarioConsultaServicio;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class EstadoUsuarioConsultaServicio implements IEstadoUsuarioConsultaServicio {
 
+    @Autowired
+    private EstadoUsuarioRepositorio estadoUsuarioRepositorio;
+
     @Override
     public EstadoUsuario buscarEstadoUsuarioPorId(Integer idEstadoUsuario) {
-        return null;
+        return this.estadoUsuarioRepositorio.findById(idEstadoUsuario).orElseThrow(()-> new RuntimeException("No se ha encontrado el estado con el id indicado"));
     }
 
     @Override
