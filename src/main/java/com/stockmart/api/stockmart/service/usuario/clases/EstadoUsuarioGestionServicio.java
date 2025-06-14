a58983a4f61e284c295babd0045dac6cc4fa614c
DeSebasWeb <juanselopezhernandez@gmail.com>
2025-06-14 16:57:03 -0500

    Add logic in methods on EstadoUsuarioGestionServicio

---
+++ src/main/java/com/stockmart/api/stockmart/service/usuario/clases/EstadoUsuarioGestionServicio.java
@@ -0,0 +1,30 @@
+package com.stockmart.api.stockmart.service.usuario.clases;
+
+import com.stockmart.api.stockmart.entity.usuario.EstadoUsuario;
+import com.stockmart.api.stockmart.repository.usuario.EstadoUsuarioRepositorio;
+import com.stockmart.api.stockmart.service.usuario.interfaces.IEstadoUsuarioGestionServicio;
+import org.springframework.beans.factory.annotation.Autowired;
+
+public class EstadoUsuarioGestionServicio implements IEstadoUsuarioGestionServicio {
+    private final EstadoUsuarioRepositorio estadoUsuarioRepositorio;
+
+    @Autowired
+    public EstadoUsuarioGestionServicio(EstadoUsuarioRepositorio estadoUsuarioRepositorio) {
+        this.estadoUsuarioRepositorio = estadoUsuarioRepositorio;
+    }
+
+    @Override
+    public EstadoUsuario guardarEstadoUsuario(EstadoUsuario estadoUsuario) {
+        return  this.estadoUsuarioRepositorio.save(estadoUsuario);
+    }
+
+    @Override
+    public EstadoUsuario actualizarEstadoUsuario(EstadoUsuario estadoUsuario) {
+        return  this.estadoUsuarioRepositorio.save(estadoUsuario);
+    }
+
+    @Override
+    public void eliminarEstadoUsuario(EstadoUsuario estadoUsuario) {
+        this.estadoUsuarioRepositorio.delete(estadoUsuario);
+    }
+}
